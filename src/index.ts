40f2ed17a1345d6cb486a9b99a92ec7fc9f585c7
Electroplayer <vitaly.hawanski@yandex.ru>
2025-10-02 22:45:56 +0300

    Теперь пары определённой группы удаляются выборочно

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import APIConvertor from './shared/lib/APIConvertor.js';
+import APIConvertor, { parseCalendar } from './shared/lib/APIConvertor.js';
 import mongoose from 'mongoose';
-import LessonModel, { ILessonSchema } from './shared/models/LessonModel.js';
+import LessonModel from './shared/models/LessonModel.js';
 
 // Сделано для определения чётности недели
 // Returns the ISO week of the date.
@@ -16,6 +16,8 @@ Date.prototype.getWeek = function() {
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGO_URI).then(() => {
     new (class Main {
+        failedGroups: { name: string, fakId: number }[] = [];
+
         constructor() {
             let p: Promise<any> = Promise.resolve(0);
 
@@ -32,8 +34,11 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
             .then(() => {
                 return this.updateZfoSchedules();
             })
-            .then(() => {
-                return mongoose.disconnect();
+            .then(async () => {
+                console.log('Не удалось для: ', this.failedGroups);
+
+                await mongoose.disconnect();
+                process.exit(0);
             });
         }
 
@@ -56,37 +61,38 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 
             if(!resp || !resp.isok) return console.log('[updater] Ошибка!', resp?.error_message);
 
-            let groups = resp.data.map((g) => ({ name: g.name, inst_id: g.inst_id }));
-            const results = await Promise.allSettled(groups.map(async (group) => {
-                try {
-                    let schedule = await APIConvertor.ofo(group.name, ugod, sem);
-                    let lessonsStartDate = await APIConvertor.parseCalendar(group.name, sem, ugod);
+            /*
+            Возможные проблемы:
+            1. Я штурмую API сразу сотнями запросов. APIConvertor при неудаче будет стараться ещё раз, что ещё увеличит количество запросов.
+            2. Если API не работает, код ниже всё равно отработает до конца, хотя смысла в этом не много
+            */
+            let groups = resp.data.map((g) => ({ name: g.name, fakId: g.inst_id }));
+            let bulk = (await Promise.all(groups.map(async (group) => {
+                let out = [];
+                let schedule = await APIConvertor.ofo(group.name, ugod, sem); // Получение расписания
+                let lessonsStartDate = await parseCalendar(group.name, sem, ugod); // Получения графика (с какого по какую дату)
 
-                    if(!schedule || !schedule.isok) {
-                        console.log(`[updater] [-] Не удалось для ${group.name}`);
-                        return [];
-                    }
-
-                    let lessons: ILessonSchema[] = schedule.data.map((l) => {
-                        if('nedType' in l.day && lessonsStartDate)
-                            l.day.weeks.startDate = new Date(lessonsStartDate.valueOf() + 1000 * 60 * 60 * 24 * 7 * (l.day.weeks.from - 1));
-
-                        return { ...l, group: group.name };
-                    });
-
-                    console.log(`[updater] [+] ${group.name}`);
-                    return lessons;
-                } catch(e) {
-                    console.log(`[updater] [!] Ошибка для ${group.name}:`, e);
+                if(!schedule?.isok) {
+                    console.log(`[updater] [-] Не удалось получить расписание для ${group.name}`);
+                    this.failedGroups.push(group);
                     return [];
                 }
-            }));
 
-            let result: ILessonSchema[] = results
-            .filter(r => r.status === 'fulfilled')
-            .flatMap(r => (r as PromiseFulfilledResult<ILessonSchema[]>).value);
+                out.push({ deleteMany: { filter: { group: group.name } } });
 
-            await LessonModel.insertMany(result).catch(console.error);
+                schedule.data.forEach((l) => {
+                    if('nedType' in l.day && lessonsStartDate)
+                        l.day.weeks.startDate = new Date(lessonsStartDate.valueOf() + 1000 * 60 * 60 * 24 * 7 * (l.day.weeks.from - 1));
+
+                    out.push({ insertOne: { document: { ...l, group: group.name } } })
+                });
+
+                console.log(`[updater] [+] ${group.name}`);
+
+                return out;
+            }))).flat();
+
+            if (bulk.length) await LessonModel.bulkWrite(bulk).catch(console.error);
         }
 
         async updateZfoSchedules() {
@@ -103,33 +109,29 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 
             if(!resp || !resp.isok) return console.log('[updater] Ошибка!', resp?.error_message);
 
-            let groups = resp.data.map((g) => ({ name: g.name, inst_id: g.inst_id }));
-            const results = await Promise.allSettled(groups.map(async (group) => {
-                try {
-                    let schedule = await APIConvertor.zfo(group.name, ugod, sem);
+            let groups = resp.data.map((g) => ({ name: g.name, fakId: g.inst_id }));
+            let bulk = (await Promise.all(groups.map(async (group) => {
+                let out = [];
+                let schedule = await APIConvertor.zfo(group.name, ugod, sem); // Получение расписания
 
-                    if(!schedule || !schedule.isok) {
-                        console.log(`[updater] [-] Не удалось для ${group.name}`);
-                        return [];
-                    }
-
-                    let lessons: ILessonSchema[] = schedule.data.map((l) => {
-                        return { ...l, group: group.name };
-                    });
-
-                    console.log(`[updater] [+] ${group.name}`);
-                    return lessons;
-                } catch(e) {
-                    console.log(`[updater] [!] Ошибка для ${group.name}:`, e);
+                if(!schedule?.isok) {
+                    console.log(`[updater] [-] Не удалось получить расписание для ${group.name}`);
+                    this.failedGroups.push(group);
                     return [];
                 }
-            }));
 
-            let result: ILessonSchema[] = results
-            .filter(r => r.status === 'fulfilled')
-            .flatMap(r => (r as PromiseFulfilledResult<ILessonSchema[]>).value);
+                out.push({ deleteMany: { filter: { group: group.name } } });
 
-            await LessonModel.insertMany(result).catch(console.error);
+                schedule.data.forEach((l) => {
+                    out.push({ insertOne: { document: { ...l, group: group.name } } })
+                });
+
+                console.log(`[updater] [+] ${group.name}`);
+
+                return out;
+            }))).flat();
+
+            if (bulk.length) await LessonModel.bulkWrite(bulk).catch(console.error);
         }
     })();
 }, console.log);
